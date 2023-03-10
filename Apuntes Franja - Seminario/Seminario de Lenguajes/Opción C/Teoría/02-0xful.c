6c89f99a989fe5bdd2a287f2fd5ee5108c2521c9
gretoide <gretaxpiacentini@gmail.com>
2023-03-10 12:34:38 -0300

    cambios

--- Apuntes Franja - Seminario/Seminario de Lenguajes/Opción C/Teoría/02-0xful.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main()
-{
-  printf("0xFUL significa: %ld\n", 0xFUL);
-  return 0;
-}
--- Apuntes Franja - Seminario/Seminario de Lenguajes/Opción C/Teoría/03-do-while.c
+++
@@ -1,7 +0,0 @@
-int main() {
-  int sumo=0, n=10;
-  do
-    sumo+=n;
-  while(--n);
-  return sumo;
-}
